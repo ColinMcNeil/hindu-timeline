1497f9938edaa4e3728e191c2928a0b3d8b608d1
colinmcneil <colinmcneil@gmail.com>
2019-02-09 15:09:50 -0500

    Add design

--- components/VPoint.vue
+++ components/VPoint.vue
@@ -1,33 +1,69 @@
 <template>
-<transition name="fade">
-  <div class="point" v-show="visible">
-    <h2>{{point.title}}</h2>
-    <p class="body">
+  <div class="point" @mouseover="mouseover" @mouseleave="mouseleave" @mousemove="mousemove" :style="{backgroundImage: `url(${point.image})`, backgroundPosition:`calc(0% + ${4*x}px) calc(0% + ${4*y}px)`}">
+    <h2><a :href="point.url" target="_black" noopener>{{point.title}}</a></h2>
+    <p class="year">Circa: {{point.year}}</p>
+    <p class="body" v-if="hovered">
       {{ point.body }}
     </p>
   </div>
-</transition>
 </template>
 <script>
 export default {
   props: {
-    visible: {type: Boolean, required: true},
     point: {type: Object, required: false, default: {}}
+  },
+  data: ()=>({x:0, y: 0, hovered: false}),
+  methods: {
+    mousemove(event) {
+      this.x = event.clientX
+      this.y = event.clientY
+    },
+    mouseleave(){
+      this.x=0
+      this.y=0
+      this.hovered=false
+    },
+    mouseover(){
+      this.hovered=true
+    }
+
   }
 }
 </script>
 <style scoped>
 .point {
-  margin-left: 400px;
-  border: solid grey 1px;
-  padding: 2em;
+  /* border: solid white 1px; */
+  border-radius: 4px;
+  padding: 10px;
   position: absolute;
-  width: 150px;
+  width: 250px;
+  height: 25%;
+  background-color: rgba(0,0,0,0.2);
+  background-size: cover;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  background-blend-mode: darken;
+  overflow: hidden;
 }
-.fade-enter-active, .fade-leave-active {
-  transition: all .4s;
+h2 {
+  margin: 0;
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  transform: scale(0);
+a {
+  color: rgb(136, 241, 255);
+  text-decoration: none;
+}
+.point:hover{
+  position: fixed;
+  width: 90%;
+  height: 90%;
+  top: 5%;
+  left: 5% !important;
+  z-index: 10;
+  font-size: 3em;
+  background-size: 1500px;
+  /* transform: translate(-10%, 0%); */
+}
+.body {
+  font-size: 2rem;
+
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,43 @@
 <template>
-  <div class="container" :style="{width: (topTimeline.length+2)*400+'px'}">
+  <div class="container" :style="{width: (topTimeline.length+1)*400+windowWidth/2+'px'}">
     <div class="section top">
+      <transition-group class="section top" name="fade">
       <v-point
         v-for="(point, index) in topTimeline"
-        :key="point.year"
-        :visible="scrollCenter-400 > index*400"
-        :style="{left: index*400+200+'px'}"
+        :key="'top-point-'+point.year"
+        v-show="scrollRight > index*400+400"
+        :style="{left: index*400+300+'px'}"
+        :scroll="scrollRight"
         :point="point"
       >
       </v-point>
+      </transition-group>
       <transition-group name="lineT">
-      <div v-for="(point, index) in bottomTimeline"
-        :key="'line'+point.year"
-        v-show="scrollCenter-420 > index*400" 
-        :style="{left: index*400+300+'px'}"
+      <div v-for="(point, index) in topTimeline"
+        :key="'top-line-'+point.year"
+        v-show="scrollRight > index*400+400" 
+        :style="{left: index*400+'px'}"
         class="vline">
       </div>
       </transition-group>
     </div>
     <div class="line" :style="{ width }" />
     <div class="section bottom">
+      <transition-group class="section bottom" name="fade">
       <v-point
         v-for="(point, index) in bottomTimeline"
-        :key="'point'+point.year"
-        :visible="scrollCenter-200 > index*400"
-        :style="{left: index*400+'px'}"
+        :key="'bottom-point-'+point.year"
+        v-show="scrollRight > index*400+200"
+        :style="{left: index*400+100+'px'}"
         :point="point"
       >
       </v-point>
+      </transition-group>
       <transition-group name="lineT">
       <div v-for="(point, index) in bottomTimeline"
-        :key="'line'+point.year"
-        v-show="scrollCenter-180 > index*400" 
-        :style="{left: index*400+100+'px'}"
+        :key="'bottom-line-'+point.year"
+        v-show="scrollRight > index*400+200" 
+        :style="{left: index*400-200+'px'}"
         class="vline">
       </div>
       </transition-group>
@@ -42,32 +47,117 @@
 <script>
 import VPoint from '~/components/VPoint'
 const timeline = [
-  { year: "500AD", body: "stuff", title:"Title" },
-  { year: "600AD", body: "stuff" },
-  { year: "700AD", body: "stuff" },
-  { year: "800AD", body: "stuff" },
-  { year: "1000AD", body: "stuff" },
-  { year: "1100AD", body: "stuff" },
-  { year: "1200AD", body: "stuff" },
-  { year: "1300AD", body: "stuff" }
+  { 
+    title: "Krishna Killing the Horse Demon Keshi", 
+    location:"Uttar Pradesh", 
+    year: "400", 
+    body: "This is a sculpture made of taracotta. \
+      It depicts Vishnu, in the farm of a warrior-king defeating \
+      the demon Krishna in the form of a horse. Early Hindu cultures \
+      believed in horse sacrifices. The viewpoint is straight on, and \
+      it's almost like a 2d painting extended into 3d space. ",
+    url: "https://www.metmuseum.org/toah/works-of-art/1991.300/",
+    image: "https://www.metmuseum.org/toah/images/hb/hb_1991.300.jpg"
+  },
+  { 
+    title: "Dashavatara Temple", 
+    location:"Deogarh, Uttar Pradesh in north-central india", 
+    year: "500", 
+    body: "Architecture: ",
+    url: "https://en.wikipedia.org/wiki/Dashavatara_Temple,_Deogarh",
+    image: "https://upload.wikimedia.org/wikipedia/commons/6/65/Deogarh01.jpg"
+  },
+  { 
+    title: "Elephanta Caves", 
+    location:"Deogarh, Uttar Pradesh in north-central india", 
+    year: "550", 
+    body: "Architecture: ",
+    url: "https://en.wikipedia.org/wiki/Elephanta_Caves",
+    image: "https://upload.wikimedia.org/wikipedia/commons/9/96/Trimurti%2C_Cave_No._1%2C_Elephanta_Caves_-_1.jpg"
+  },
+  { 
+    title: "Descent of the Ganges", 
+    location:"Deogarh, Uttar Pradesh in north-central india", 
+    year: "650", 
+    body: "Architecture: ",
+    url: "https://en.wikipedia.org/wiki/Descent_of_the_Ganges_(Mahabalipuram)",
+    image: "https://upload.wikimedia.org/wikipedia/commons/6/6b/India_-_Mamallapuram_-_030_-_Arjunas_Penance_Bas_Relief_%28490988077%29.jpg"
+  },
+  { 
+    title: "Shiva as Lord of Dance (Nataraja)", 
+    location:"Deogarh, Uttar Pradesh in north-central india", 
+    year: "1000", 
+    body: "Architecture: ",
+    url: "https://www.metmuseum.org/art/collection/search/39328",
+    image: "https://images.metmuseum.org/CRDImages/as/original/DT240.jpg"
+  },
+  { 
+    title: "Vajravarahi in Wrathful Posture", 
+    location:"Central Tibet", 
+    year: "1200", 
+    body: "Architecture: ",
+    url: "https://www.metmuseum.org/art/collection/search/665922",
+    image: "https://images.metmuseum.org/CRDImages/as/original/DP702272.jpg"
+  },
+  { 
+    title: "Durga as Slayer of the Buffalo Demon Mahishasura", 
+    location:"Nepal", 
+    year: "1350", 
+    body: "Architecture: ",
+    url: "https://www.metmuseum.org/toah/works-of-art/1986.498/",
+    image: "https://www.metmuseum.org/toah/images/hb/hb_1986.498.jpg"
+  },
+  { 
+    title: "Head of Bhairava", 
+    location:"Nepal, Kathmandu Valley", 
+    year: "1500", 
+    body: "Architecture: ",
+    url: "https://www.metmuseum.org/art/collection/search/78187",
+    image: "https://images.metmuseum.org/CRDImages/as/original/DP307219.jpg"
+  },
+  { 
+    title: "Melancholy Courtesan", 
+    location:"India (Rajasthan, Bundi, or Kota [?])", 
+    year: "1750", 
+    body: "Architecture: ",
+    url: "https://www.metmuseum.org/art/collection/search/38012",
+    image: "https://images.metmuseum.org/CRDImages/as/original/DT3878.jpg"
+  },
+  { 
+    title: "Kali", 
+    location:"Kolkata, India", 
+    year: "1885", 
+    body: "Architecture: ",
+    url: "https://www.metmuseum.org/art/collection/search/78257",
+    image :"https://images.metmuseum.org/CRDImages/as/original/DP702072.jpg"
+  },
+  { 
+    title: "Lakshmi", 
+    location:"India", 
+    year: "1894", 
+    body: "Architecture: ",
+    url: "https://www.metmuseum.org/art/collection/search/78264",
+    image: "https://images.metmuseum.org/CRDImages/as/original/DP702078.jpg"
+  },
+
 ]
 export default {
   components: {
     VPoint
   },
-  data: () => ({ scroll: 0, timeline, windowWidth: 0 }),
+  data: () => ({ scroll: 10, timeline, windowWidth: 0 }),
   computed: {
     width() {
-      return `calc(73vw + ${this.scroll}px)`
+      return `calc(50vw + ${this.scroll}px)`
     },
-    scrollCenter(){
-      return this.windowWidth/2 + this.scroll
+    scrollRight(){
+      return this.windowWidth*0.6 + this.scroll
     },
     topTimeline(){
-      return timeline.filter((elem, index) => index % 2 == 0)
+      return timeline.filter((elem, index) => index % 2 != 0)
     },
     bottomTimeline(){
-      return timeline.filter((elem, index) => index % 2 != 0)
+      return timeline.filter((elem, index) => index % 2 == 0)
     }
   },
   created() {
@@ -88,10 +178,12 @@ export default {
 }
 </script>
 <style>
-html,
+@import url('https://fonts.googleapis.com/css?family=Nanum+Myeongjo');
 html, body {
   margin: 0;
   overflow-y: hidden;
+  font-family: 'Nanum Myeongjo', serif;
+  color: white;
 }
 </style>
 
@@ -102,20 +194,30 @@ html, body {
   justify-content: space-around;
   position: absolute;
   height: 100%;
-  
+  /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#1e5799+0,efa12b+100 */
+  background: #1e5799; /* Old browsers */
+  background: -moz-linear-gradient(left, #1e5799 0%, #efa12b 100%); /* FF3.6-15 */
+  background: -webkit-linear-gradient(left, #1e5799 0%,#efa12b 100%); /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(to right, #1e5799 0%,#efa12b 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1e5799', endColorstr='#efa12b',GradientType=1 ); /* IE6-9 */
 }
 .section {
   display: flex;
   flex-basis: 100%;
   height: 100%;
-  padding: 3em;
+}
+.top {
+  align-items: flex-end;
+  margin-bottom: 60px;
 }
 .bottom {
-  align-items: flex-end;
+  margin-top: 60px
 }
 .line {
-  height: 5px;
-  background: black;
+  height: 7px;
+  background: orange;
+  border-top: solid blue 1px;
+  border-bottom: solid blue 1px;
   top: 50%;
   left: 20px;
   transition: ease 0.2s all;
@@ -123,9 +225,11 @@ html, body {
 .vline {
   display: block;
   position: absolute;
-  width: 2px;
+  width: 4px;
   height: 100px;
-  background: black;
+  background: orange;
+  border-left: solid blue 1px;
+  border-right: solid blue 1px;
   margin-left: 400px;
   bottom: 50%;
 }
@@ -141,6 +245,13 @@ html, body {
 }
 .lineT-enter, .lineT-leave-to /* .fade-leave-active below version 2.1.8 */ {
   height: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: ease transform .4s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  transform: scale(0);
 }
 
 </style>
